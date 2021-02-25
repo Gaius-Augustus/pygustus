819a804740fb83a5ead88181ceb925065c365365
Daniel Honsel <dhonsel@informatik.uni-goettingen.de>
2021-02-25 10:10:15 +0100

    improved test case execution

--- tests/test_augustus.py
+++ tests/test_augustus.py
@@ -1,21 +1,34 @@
-import unittest
+#!/usr/bin/env python3
 
+import unittest
 import augustus
 
 
 class TestAugustus(unittest.TestCase):
 
-    def setUp(self):
-        print('-------------------------------')
-
-    def tearDown(self) -> None:
-        print('-------------------------------')
-
     def test_augustus_help(self):
         augustus.run('--help')
 
+    def test_augustus_simple(self):
+        augustus.run('data/example.fa', species='human',
+                     UTR='on', softmasking=False)
+
     def test_augustus_wrong_parameter(self):
         with self.assertRaises(ValueError) as cm:
-            augustus.run('--unknown=nothing')
+            augustus.run('data/example.fa', species='human',
+                         UTR='on', smasking=False)
         ex = cm.exception
         self.assertEqual(ex.error_code, 3)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestAugustus('test_augustus_help'))
+    suite.addTest(TestAugustus('test_augustus_simple'))
+    suite.addTest(TestAugustus('test_augustus_wrong_parameter'))
+    return suite
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite())
