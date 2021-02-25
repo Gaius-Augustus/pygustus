32144f5d61e4a9ae385ede32439477c642543e66
Daniel Honsel <dhonsel@informatik.uni-goettingen.de>
2021-02-25 13:27:02 +0100

    use pytest instead of unittest

--- tests/test_augustus.py
+++ tests/test_augustus.py
@@ -1,40 +1,17 @@
-#!/usr/bin/env python3
-
-import unittest
-import os
-import augustus
+import pytest
+from augustus import *
 
 
-class TestAugustus(unittest.TestCase):
+def test_augustus_help():
+    augustus.run('--help')
 
-    def test_augustus_help(self):
-        augustus.run('--help')
 
-    def test_augustus_simple(self):
+def test_augustus_simple():
+    augustus.run('data/example.fa', species='human',
+                 UTR='on', softmaskingg=False)
+
+
+def test_augustus_wrong_parameter():
+    with pytest.raises(ValueError):
         augustus.run('data/example.fa', species='human',
-                     UTR='on', softmasking=False)
-
-    def test_augustus_wrong_parameter(self):
-        with self.assertRaises(ValueError) as cm:
-            augustus.run('data/example.fa', species='human',
-                         UTR='on', smasking=False)
-        ex = cm.exception
-        self.assertEqual(ex.error_code, 3)
-
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestAugustus('test_augustus_help'))
-    suite.addTest(TestAugustus('test_augustus_simple'))
-    suite.addTest(TestAugustus('test_augustus_wrong_parameter'))
-    return suite
-
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner(verbosity=2)
-    result = runner.run(suite())
-
-    if result.wasSuccessful():
-        os._exit(0)
-    else:
-        os._exit(1)
+                     UTR='on', smasking=False)
