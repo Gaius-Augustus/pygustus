a13635e276ccd275ca906248cbfaa740a79906e8
Daniel Honsel <daniel.honsel@gmail.com>
2021-03-18 13:21:12 +0100

    add some simple test cases

--- tests/test_augustus.py
+++ tests/test_augustus.py
@@ -17,7 +17,19 @@ def test_augustus_wrong_parameter():
                          UTR=True, smasking=False)
 
 
+def test_augustus_wrong_parameter():
+    with pytest.raises(ValueError):
+        augustus.predict('data/example.fa', species='human',
+                         UTR=True, softmasking=0.42)
+
+
 def test_augustus_wrong_bin_path():
     with pytest.raises(RuntimeError):
         augustus.predict('data/example.fa', species='human',
                          UTR=True, softmasking=False, path_to_bin='/usr/local/bin')
+
+
+def test_augustus_wrong_bin_path_format():
+    with pytest.raises(ValueError):
+        augustus.predict('data/example.fa', species='human',
+                         UTR=True, softmasking=False, path_to_bin=False)
--- tests/test_etraining.py
+++ tests/test_etraining.py
@@ -6,7 +6,23 @@ def test_etraining_simple():
     etraining.train('data/genes.gb.train', species='bug', softmasking=False)
 
 
+def test_etraining_wrong_parameter():
+    with pytest.raises(ValueError):
+        etraining.train('data/genes.gb.train', species='bug', smasking=False)
+
+
+def test_etraining_wront_parameter_format():
+    with pytest.raises(ValueError):
+        etraining.train('data/genes.gb.train', species=True, softmasking=False)
+
+
 def test_etraining_wrong_bin_path():
     with pytest.raises(RuntimeError):
         etraining.train('data/genes.gb.train', species='bug',
-                         softmasking=False, path_to_bin='/usr/local/bin')
+                        softmasking=False, path_to_bin='/usr/local/bin')
+
+
+def test_etraining_wrong_bin_path_format():
+    with pytest.raises(ValueError):
+        etraining.train('data/genes.gb.train', species='bug',
+                        softmasking=False, path_to_bin=42)
