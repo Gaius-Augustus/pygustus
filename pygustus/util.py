b2fb4c1a96f72dd6a670bb3655cc27bd03cf93c9
Daniel Honsel <daniel.honsel@gmail.com>
2021-03-18 13:37:56 +0100

    check returncode

--- pygustus/util.py
+++ pygustus/util.py
@@ -6,13 +6,15 @@ from pygustus.options.aug_options import *
 
 def execute_bin(cmd, options):
     # execute given binary with given options
-    process = subprocess.Popen(
+    rc = process = subprocess.Popen(
         [cmd] + options, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
     output = process.stdout.read()
     error = process.stderr.read()
     print(output)
     print(error)
+    if rc != 0:
+        print(f'Unexpected returncode {rc}!')
 
 
 def check_bin(bin):
