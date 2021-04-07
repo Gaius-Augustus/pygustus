5f88f251fc59f5ef8ccdee04a8a827862bf8edae
Daniel Honsel <daniel.honsel@gmail.com>
2021-04-07 10:16:52 +0200

    add method to split fasta files based on splitMfasta.pl

--- pygustus/fasta_methods.py
+++ pygustus/fasta_methods.py
@@ -1,3 +1,4 @@
+import os
 from Bio import SeqIO
 
 
@@ -56,3 +57,23 @@ def summarize_acgt_content(inputfile):
 def update_values(file_sum, key, value):
     cur_value = file_sum[key]
     file_sum.update({key: cur_value + value})
+
+
+def split(inputfile, outputpath, minsize=0):
+    fileidx = 0
+    cursize = 0
+    records_to_write = list()
+    records = list(SeqIO.parse(inputfile, 'fasta'))
+
+    for seq_record in records:
+        cursize += len(seq_record)
+        records_to_write.append(seq_record)
+        if minsize == 0 or cursize >= minsize or seq_record.id == records[-1].id:
+            fileidx += 1
+            filename = os.path.basename(inputfile)
+            f_name, f_ext = os.path.splitext(filename)
+            s_filename = f'{f_name}.split.{fileidx:05d}{f_ext}'
+            splitpath = os.path.join(outputpath, s_filename)
+            SeqIO.write(records_to_write, splitpath, 'fasta')
+            cursize = 0
+            records_to_write.clear()
