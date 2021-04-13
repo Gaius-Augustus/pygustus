1d77bc2ecc3c9ce58374ea08939057010d6f8c66
Daniel Honsel <daniel.honsel@gmail.com>
2021-04-13 14:50:43 +0200

    adapt paths for etraining tests

--- tests/test_etraining.py
+++ tests/test_etraining.py
@@ -3,26 +3,26 @@ from pygustus import *
 
 
 def test_etraining_simple():
-    etraining.train('data/genes.gb.train', species='bug', softmasking=False)
+    etraining.train('tests/data/genes.gb.train', species='bug', softmasking=False)
 
 
 def test_etraining_wrong_parameter():
     with pytest.raises(ValueError):
-        etraining.train('data/genes.gb.train', species='bug', smasking=False)
+        etraining.train('tests/data/genes.gb.train', species='bug', smasking=False)
 
 
 def test_etraining_wront_parameter_format():
     with pytest.raises(ValueError):
-        etraining.train('data/genes.gb.train', species=True, softmasking=False)
+        etraining.train('tests/data/genes.gb.train', species=True, softmasking=False)
 
 
 def test_etraining_wrong_bin_path():
     with pytest.raises(RuntimeError):
-        etraining.train('data/genes.gb.train', species='bug',
+        etraining.train('tests/data/genes.gb.train', species='bug',
                         softmasking=False, path_to_bin='/usr/local/bin')
 
 
 def test_etraining_wrong_bin_path_format():
     with pytest.raises(ValueError):
-        etraining.train('data/genes.gb.train', species='bug',
+        etraining.train('tests/data/genes.gb.train', species='bug',
                         softmasking=False, path_to_bin=42)
