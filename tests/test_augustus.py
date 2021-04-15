1a4092eb17a651d322eb005b1f3c1d8bc9d5d344
Daniel Honsel <daniel.honsel@gmail.com>
2021-04-15 11:32:53 +0200

    add gff3 test case

--- tests/test_augustus.py
+++ tests/test_augustus.py
@@ -78,6 +78,45 @@ def test_augustus_parallel_two_jobs_and_sequences():
     assert diff == ''
 
 
+def test_augustus_parallel_two_jobs_and_sequences_gff3():
+    outdir = os.path.join(
+        'tests/out', test_augustus_parallel_two_jobs_and_sequences_gff3.__name__)
+    out_html = os.path.join(outdir, 'output_html')
+    out_augustus_tmp = os.path.join(outdir, 'augustus_tmp.gff')
+    out_augustus_joined_tmp = os.path.join(outdir, 'augustus_joined_tmp.gff')
+    out_augustus = os.path.join(outdir, 'augustus.gff')
+    out_augustus_joined = os.path.join(outdir, 'augustus_joined.gff')
+
+    if (os.path.exists(outdir)):
+        shutil.rmtree(outdir)
+    os.makedirs(outdir)
+
+    assert os.path.exists(outdir)
+
+    # run augustus
+    augustus.predict('tests/data/example.fa', species='human',
+                     UTR=True, softmasking=False, gff3=True,
+                     outfile=out_augustus_tmp)
+    assert os.path.exists(out_augustus_tmp)
+
+    # run augustus with two jobs on the same input file
+    augustus.predict('tests/data/example.fa', species='human', jobs=2,
+                     UTR=True, softmasking=False, gff3=True,
+                     outfile=out_augustus_joined_tmp)
+    assert os.path.exists(out_augustus_joined_tmp)
+
+    # filter both results
+    afilter.pred(out_augustus_tmp, out_augustus)
+    afilter.pred(out_augustus_joined_tmp, out_augustus_joined)
+    os.remove(out_augustus_tmp)
+    os.remove(out_augustus_joined_tmp)
+
+    # compare results
+    diff = comp.compare_files(
+        out_augustus, out_augustus_joined, html=True, outputfolder=out_html)
+    assert diff == ''
+
+
 def test_augustus_parallel_one_large_sequence():
     outdir = os.path.join(
         'tests/out', test_augustus_parallel_one_large_sequence.__name__)
