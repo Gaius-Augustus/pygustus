fdaaca9e1d1dbd69db2274665c16c04cd53df704
Daniel Honsel <dhonsel@informatik.uni-goettingen.de>
2021-08-23 13:14:43 +0200

    prepare for 0.7.0 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pygustus',
-    version='0.6.0',
+    version='0.7.0',
     description='Python wrapper for AUGUSTUS.',
     long_description=long_description,
     long_description_content_type='text/markdown',
